d0d9dcd8635e4083e24ba69434ce4fe377ec8c8f
Thomas Edwin Bischoff <149975089+edwinbischoff@users.noreply.github.com>
2025-10-11 01:30:02 -0400

    Update bot.js (cleanup

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
+import 'dotenv/config';
 import { Client, GatewayIntentBits } from "discord.js";
+
+const TOKEN = process.env.TOKEN;
+const TARGET_ROLE_ID = "1426431045905154152";
 
 const client = new Client({
   intents: [
@@ -8,27 +12,16 @@ const client = new Client({
   ],
 });
 
-import 'dotenv/config';  // if using a .env file locally
-const TOKEN = process.env.TOKEN;
-
-const TARGET_ROLE_ID = "1426431045905154152";     // replace with the role you want to auto-remove
-
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("guildMemberUpdate", async (oldMember, newMember) => {
-  // detect newly added roles
-  const addedRoles = newMember.roles.cache.filter(
-    (role) => !oldMember.roles.cache.has(role.id)
-  );
+  const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
 
-  // if target role added
   if (addedRoles.has(TARGET_ROLE_ID)) {
     try {
-      // remove the role
       await newMember.roles.remove(TARGET_ROLE_ID);
-      // send a DM
       await newMember.send("Your role was automatically removed after being given.");
       console.log(`Removed role from ${newMember.user.tag}`);
     } catch (error) {
